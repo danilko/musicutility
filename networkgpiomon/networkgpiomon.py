c7314c3d341624bdebb93f31693f145d9fb7926f
Danil Ko <danilko@localhost.localdomain>
2020-08-27 14:22:39 -0700

    Update GPIO deamon for enabling/disabling network base on GPIO input

--- networkgpiomon/networkgpiomon.py
+++ networkgpiomon/networkgpiomon.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 # Apache-2.0 License
 # https://github.com/danilko/musicutility
 
@@ -11,38 +11,42 @@ import os
 
 if __name__ == '__main__':
     def get_event():
+        global currentValue
         event_lines = lines.event_wait(sec=1)
         if event_lines and len(event_lines) > 0:
             event = event_lines[0].event_read()
-            if event.type == gpiod.LineEvent.RISING_EDGE:
-                print("DEAMON NETWORKING ON")
-                os.system('sudo nmcli networking on')
-            elif event.type == gpiod.LineEvent.FALLING_EDGE:
-                print("DEAMON NETWORKING OFF")
-                os.system('sudo nmcli networking off')
-            else:
-                raise TypeError("Invalid Event")
+            if event.type == gpiod.LineEvent.FALLING_EDGE:
+                function = "on"
+                if currentValue == 0:
+                    currentValue = 1
+                else:
+                    currentValue = 0
+                    function = "off"
+                os.system("sudo nmcli networking {}".format(function))
+            elif not event.type == gpiod.LineEvent.RISING_EDGE:
+                print("Invalid Event")
 
     def get_value():
+        global currentValue
         values = lines.get_values()
         if values and len(values) > 0:
+            function = "on"
             if values[0] == 0:
-                print("INITIAL NETWORKING OFF")
-                os.system('sudo nmcli networking off')
+                currentValue = values[0]
             elif values[0] == 1:
-                print("INITIAL NETWORKING ON")
-                os.system('sudo nmcli networking on')
+                currentValue = values[0]
+                function = "on"
             else:
-                raise TypeError("Invalid Value")
+                print("Invalid Value")
 
-
+            os.system("sudo nmcli networking {}".format(function))
     if len(sys.argv) < 3:
         raise TypeError("Incorrect parameter\nUsage wifigpiomon.py <chip> <offset>")
     
     consumer=sys.argv[0]
     chip=sys.argv[1]
     offset=sys.argv[2]
-
+    
     if not chip.isnumeric():
         raise TypeError("Chip must be a number\nUsage wifigpiomon.py <chip> <offset>")
     
@@ -50,14 +54,16 @@ if __name__ == '__main__':
         raise TypeError("Offset must be a number\nUsage wifigpiomon.py <chip> <offset>")
 
     with gpiod.Chip(chip) as chip:
+        currentValue = 1
+
         lines = chip.get_lines([int(offset)])
 
         lines.request(consumer=consumer, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
 
         try:
             get_value()
-
             while True:
                 get_event()
         except KeyboardInterrupt:
             sys.exit(0)
+
