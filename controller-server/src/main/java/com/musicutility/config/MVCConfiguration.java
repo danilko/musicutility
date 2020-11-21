be512aeab088baa4de2e127c2061e92323a25a9a
Danil Ko <danilko@localhost.localdomain>
2020-11-21 00:45:09 -0800

    https://github.com/danilko/musicutility/issues/16: Fix the path routing problem for app

---
+++ controller-server/src/main/java/com/musicutility/config/MVCConfiguration.java
@@ -0,0 +1,31 @@
+package com.musicutility.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+// Reference https://keepgrowing.in/java/springboot/make-spring-boot-surrender-routing-control-to-angular/
+
+@Configuration
+public class MVCConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/resources/index.html");
+                    }
+                });
+    }
+}
